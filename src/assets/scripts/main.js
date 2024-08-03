f4dc7e6ee4da64bca21f5049edd9a40e5680a51f
Yichuan Shen <shen.yichuan@gmail.com>
2024-08-03 20:42:09 +0200

    Dynamically create side navigation

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,4 +1,5 @@
 const $ = (x) => document.querySelectorAll(x);
+const h = (x) => document.createElement(x);
 
 // Mark current main navigation entry
 
@@ -21,4 +22,29 @@ if ($(".hero-banner").length > 0) {
   for (const li of $(".main-nav .icon")) {
     li.remove();
   }
+}
+
+// Create side navigation
+
+const sections = [...$(".main-content .inner h2")].filter((h2) => !!h2.id);
+
+if (sections.length > 0 && $(".main-content .side-nav").length === 0) {
+  const nav = h("nav");
+  const ul = h("ul");
+
+  nav.classList.add("side-nav");
+  nav.append(ul);
+
+  for (const section of sections) {
+    const li = h("li");
+    const a = h("a");
+
+    a.textContent = section.textContent;
+    a.href = "#" + section.id;
+
+    li.append(a);
+    ul.append(li);
+  }
+
+  $(".main-content")[0].prepend(nav);
 }
